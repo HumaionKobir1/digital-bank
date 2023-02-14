be0e7bf3ab412ac54210db760affd7ab3c973cc1
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-14 18:34:48 +0600

    Added => deposit and withdraw area

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function LoginBtn () {
-  const bannerArea = document.getElementById('banner-area');
-  bannerArea.style.display = 'none';
+    const email = document.getElementById('user-email').value;
+    const password = document.getElementById('user-password').value;
+//   const bannerArea = document.getElementById('banner-area');
+//   bannerArea.style.display = 'none';
+console.log(email, password)
+
+    // Danger: DO NOT VERIFY email password on the client side
+    if (email === 'humaionkobir@gmail.com' && password === '12345678'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        console.log('invalid user');
+    }
 }
 
     
