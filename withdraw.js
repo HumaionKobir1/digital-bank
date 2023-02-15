d321b5d1850740c01d8f4fd895162bd275a313e2
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-15 11:12:02 +0600

    Added => validation for this function

--- withdraw.js
+++ withdraw.js
@@ -2,10 +2,29 @@ function withdrawBtn () {
     const withdrawField = document.getElementById('withdraw-field').value;
     const newWithdrawAmount = parseFloat(withdrawField);
 
+    document.getElementById('withdraw-field').value = '';    
+    if(isNaN(newWithdrawAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
+
     const withdrawTotal = document.getElementById('withdraw-total').innerText;
     const previousWithdrawAmount = parseFloat(withdrawTotal);
+
+    const balanceTotalElment = document.getElementById('balance-total').innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalElment);
+
+    document.getElementById('withdraw-field').value = '';
+
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert("Your account doesn't have enough money");
+        return;
+    }
+
     const currentTotalWithdraw = newWithdrawAmount + previousWithdrawAmount;
     document.getElementById('withdraw-total').innerText = currentTotalWithdraw;
 
-    document.getElementById('withdraw-field').value = '';
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    document.getElementById("balance-total").innerText = newBalanceTotal;
+
 }
