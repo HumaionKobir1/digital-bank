a6a288ffaa551ceb6af5048cdee85050ceee319a
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-15 12:54:26 +0600

    some issu fixed this bank website

--- login.js
+++ login.js
@@ -6,13 +6,13 @@ function LoginBtn () {
 
     // Danger: DO NOT VERIFY email password on the client side
     if (email === 'humaionkobir@gmail.com' && password === '12345678'){
-        window.location.href = 'bank.html';
+        const mainContainer = document.getElementById('main-container');
+        mainContainer.style.display = 'block';
+
+        const bannerArea = document.getElementById('banner-area');
+        bannerArea.style.display = 'none';
     }
     else{
         console.log('invalid user');
     }
 }
-
-    
-
-
